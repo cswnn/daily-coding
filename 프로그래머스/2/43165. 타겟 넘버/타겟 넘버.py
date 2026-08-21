bd35547dfbd1a646fa7a4a32287406085af029ae
Sewon <150823023+cswnn@users.noreply.github.com>
2026-08-21 15:57:30 +0900

    [level 2] Title: 타겟 넘버, Time: 113.73 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -4,6 +4,7 @@ def solution(numbers, target):
     # n개의 수를 순서 그대로 더하거나 빼서 target 만들고, 가능한 방법 수 리턴
     # 길이: 2이상 20이하, 각 숫자: 1이상 50이하, 타겟: 1이상 1000이하
     
+    # DFS 활용
     def dfs(present, total):
         if present < len(numbers):
             return dfs(present + 1, total + numbers[present]) \
