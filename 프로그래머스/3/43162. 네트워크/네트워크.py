d724f6bf20d1e1a55eaf69fe009297c277686cd4
Sewon <150823023+cswnn@users.noreply.github.com>
2026-09-03 21:06:48 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,20 @@
+def solution(n, computers):
+    answer = 0
+    
+    ## 네트워크(연결된 덩어리) 개수 리턴
+    
+    visited = [0] * n
+    
+    def dfs(v):
+        visited[v] = 1 # 방문 처리
+        for next_v in range(n):
+            # 연결 O, 방분 X
+            if not visited[next_v] and computers[v][next_v]:
+                dfs(next_v)
+
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+    
+    return answer
